e999233a02b65b1f3276b7c3d0f9d984d12d8d1a
Aluno <you@example.com>
2019-06-25 21:31:58 -0300

    Implementação do Redirect

--- gestao/views.py
+++ gestao/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
 
 def home(request):
-    return render(request, 'home/home.html')
-    #if request.user.is_authenticated:
-    #    return render(request, 'home/home.html')
-    #else:
-    #    return redirect("/accounts/login/")
+    if request.user.is_authenticated:
+        return render(request, 'home/home.html')
+    else:
+        return redirect("/accounts/login/")
