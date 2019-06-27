5e7aad81bc3e4e1c17f9e895393cf0ee80b762ce
Aluno <you@example.com>
2019-06-27 20:17:32 -0300

    Implementação da extenção da class User

--- gestao/forms.py
+++ gestao/forms.py
@@ -1,11 +1,23 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import UserProfile
 from localflavor.br.forms import BRCPFField
 
-class login(UserCreationForm):
-    cpf = BRCPFField(required=True, max_length=11)
-
+class ExtendsUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+
+        if commit:
+            user.save()
+        return user
+
+class UserProfileForm(forms.ModelForm):
+    cpf = BRCPFField(required=True, max_length=11)
+    class Meta:
+        model = UserProfile
+        fields = ("cpf",)
--- gestao/views.py
+++ gestao/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render, redirect
-from . forms import login
+from . forms import ExtendsUserCreationForm, UserProfileForm
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
 def register(request):
     if request.method == "POST":
-        form = login(request.POST)
-        if form.is_valid():
-            form.save()
+        form = ExtendsUserCreationForm(request.POST)
+        form2 = UserProfileForm(request.POST)
+        if form.is_valid() and form2.is_valid():
+            user = form.save()
+            profile = form2.save(commit=False)
+            profile.user = user
+            profile.save()
             return redirect('/accounts/login/')
         else:
-            return render(request, 'registration/register.html', {"form":form})
+            return render(request, 'registration/register.html', {"form":form, "form2":form2})
     else:
-        form = login()
-        return render(request, 'registration/register.html', {"form":form})
+        form = ExtendsUserCreationForm()
+        form2 = UserProfileForm()
+        return render(request, 'registration/register.html', {"form":form, "form2":form2})
 
 def home(request):
     if request.user.is_authenticated:
