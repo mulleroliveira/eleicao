ebf8d9eab72f30c10bb9dc5e521a03033ed62eb3
Muller <you@example.com>
2019-06-27 16:52:37 -0300

    Pequeno detalhe na class Login

--- gestao/forms.py
+++ gestao/forms.py
@@ -5,8 +5,7 @@ from localflavor.br.forms import BRCPFField
 
 class login(UserCreationForm):
     cpf = BRCPFField(required=True, max_length=11)
-    email = forms.EmailField(max_length=254)
 
     class Meta:
         model = User
-        fields = ("username", "cpf", "email", "password1", "password2")
+        fields = ("username", "email", "password1", "password2")
